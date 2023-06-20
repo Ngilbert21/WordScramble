dbb5ed05c83880f587144defffa138cf4b48de1f
Nicholas Gilbert <nicholasgilbert21@yahoo.com>
2023-06-20 13:19:56 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Nicholas Gilbert on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
